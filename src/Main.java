db04734fb1b4fc32babeef64f700daf0bb62703e
Denchik1206 <denisdanilov03@gmail.com>
2024-11-06 23:46:13 +0300

    v1.1

--- src/Main.java
+++ src/Main.java
@@ -16,71 +16,85 @@ public class Main {
 
         boolean[] max_island = new boolean[0];
         int max_len_islant = 0;
+        int cnt_islant = 0;
 
-        boolean[] swap;
+        int[][] cnt_true_max_island = new int[matrix.length][2];
         for (int i = 0; i < matrix.length; i++) {
-            int cnt_true = 0;
-            int first_cnt_true = -1;
+            int cnt_true_i = 0;
             for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] == true)
-                    cnt_true += 1;
+                    cnt_true_i += 1;
             }
+            cnt_true_max_island[i][0] = cnt_true_i;
 
-            int max_island_in_first = 0;
             int max_island_in_i = 0;
             int lenn_island = 1;
             for (int j = 0; j < matrix[i].length - 1; j++) {
-                if (matrix[i][j] == matrix[i][j + 1])
+                if (matrix[i][j] == matrix[i][j + 1] && matrix[i][j] == true)
                     lenn_island += 1;
-                else if (lenn_island != 1 && ((matrix[i][j] != matrix[i][j + 1]) && (matrix[i][j - 1] == matrix[i][j]))) {
+                else if (lenn_island != 1 && (matrix[i][j - 1] == true && matrix[i][j] == true)) {
                     if (max_island_in_i == 0 || max_island_in_i < lenn_island)
                         max_island_in_i = lenn_island;
+                    cnt_islant += 1;
                     lenn_island = 1;
                 }
             }
+            cnt_true_max_island[i][1] = max_island_in_i;
+
             if (max_island_in_i > max_len_islant) {
                 max_len_islant = max_island_in_i;
                 max_island = matrix[i];
             }
-            if (max_island_in_first == 0)
-                max_island_in_first = max_island_in_i;
+        }
 
-
-            if (first_cnt_true == -1) {
-                first_cnt_true = cnt_true;
-                continue;
-            } else if (cnt_true >= first_cnt_true) {
-                if (cnt_true > first_cnt_true) {
+        for (int k = 0; k < matrix.length; k++) {
+            for (int i = 0; i < cnt_true_max_island.length - 1; i++) {
+                boolean[] swap;
+                int[] swap1;
+                if (cnt_true_max_island[i + 1][0] > cnt_true_max_island[i][0]) {
                     swap = matrix[i];
-                    for (int k = i; k > 0; k++)
-                        matrix[k] = matrix[k - 1];
-                    matrix[0] = swap;
-                } else {
-                    if (max_island_in_i > max_island_in_first) {
+                    matrix[i] = matrix[i + 1];
+                    matrix[i + 1] = swap;
+
+                    swap1 = cnt_true_max_island[i];
+                    cnt_true_max_island[i] = cnt_true_max_island[i + 1];
+                    cnt_true_max_island[i + 1] = swap1;
+                } else if (cnt_true_max_island[i + 1][0] == cnt_true_max_island[i][0]) {
+                    if (cnt_true_max_island[i + 1][1] > cnt_true_max_island[i][1]) {
                         swap = matrix[i];
-                        for (int k = i; k > 0; k++)
-                            matrix[k] = matrix[k - 1];
-                        matrix[0] = swap;
-                    } else {
-                        swap = matrix[i];
-                        for (int k = i; k > 1; k++)
-                            matrix[k] = matrix[k - 1];
-                        matrix[1] = swap;
+                        matrix[i] = matrix[i + 1];
+                        matrix[i + 1] = swap;
+
+                        swap1 = cnt_true_max_island[i];
+                        cnt_true_max_island[i] = cnt_true_max_island[i + 1];
+                        cnt_true_max_island[i + 1] = swap1;
                     }
-
                 }
-
             }
         }
 
+        // (Временно) Для тест вывода
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++)
                 out.print(matrix[i][j] + " ");
             out.println();
         }
-        
+        out.println();
+
+
         for (int i = 0; i < max_island.length; i++)
             out.print(max_island[i] + " ");
-        out.println(max_len_islant);
+        out.printf("\nДлина: %d\n\n", max_len_islant);
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j])
+                    out.print("+" + " ");
+                else
+                    out.print("-" + " ");
+            }
+            out.println();
+        }
+        out.printf("Число островков: %d\n", cnt_islant);
     }
 }
